4e511ae5f1e7c647591a001ca9d12959831ae6cb
= <=>
2017-03-20 15:44:29 -0700

    1st attempt at pipe

--- src/Pipe.cpp
+++ src/Pipe.cpp
@@ -10,5 +10,53 @@ void Pipe::setLeft(Input* l) { left = l; }
 void Pipe::setRight(Input* r) { right = r; }
 
 bool Pipe::evaluate() {
-	return false;
+	// 1) pipe
+    int pipe1[2];
+    pipe(pipe1);
+    
+    // 2) overwrite standard output -- SAVE OUTPUT
+    int saveOut = dup(1);
+    if (saveOut == -1) {
+        perror("dup out");
+        return false;
+    }
+    if (dup2(pipe1[1], 1) == -1) {
+        perror("out dup2");
+        return false;
+    }
+
+    // 3) run left
+    bool leftside = left->evaluate();
+
+    // 4) restore standard output
+    if (dup2(saveOut, 1) == -1) {
+        perror("leftside restore");
+        return false;
+    }
+
+    // 5) overwrite standard input -- SAVE
+    int saveIn = dup(0);
+    if (saveIn == -1) {
+        perror("dup in");
+        return false;
+    }
+    if (dup2(pipe1[0], 0) == -1) {
+        perror("in dup2");
+        return false;
+    }
+
+    // 6) call right
+    bool rightside = right->evaluate();
+
+    // 7) restore standard input
+    if (dup2(saveIn, 0) == -1) {
+        perror("rightside restore");
+        return false;
+    }
+
+    // 8) return value
+    if (leftside && rightside)
+        return true;
+    else
+        return false;
 }
