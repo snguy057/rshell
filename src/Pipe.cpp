883a9a17ea71028504e56ed4a83946587a664e8f
= <=>
2017-03-20 18:15:08 -0700

    attempting new pipe fix

--- src/Pipe.cpp
+++ src/Pipe.cpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <string.h>
 #include <vector>
+#include <iostream>
 #include "../header/Pipe.h"
 
 Pipe::Pipe() : left(0), right("") {}
@@ -18,31 +19,6 @@ bool Pipe::evaluate() {
     int pipe1[2];
     pipe(pipe1);
     
-    // 2) overwrite standard output -- SAVE OUTPUT
-    int saveOut = dup(1);
-    if (saveOut == -1) {
-        perror("dup out");
-        return false;
-    }
-    if (dup2(pipe1[1], 1) == -1) {
-        perror("out dup2");
-        return false;
-    }
-
-    // 3) run left
-    bool leftside = left->evaluate();
-
-    if (!leftside) {
-        return false;
-    }
-
-    // 4) restore standard output
-    if (dup2(saveOut, 1) == -1) {
-        perror("leftside restore");
-        return false;
-    }
-
-    // FIXME BELOW
     // Removing leading whitespace
     if (right.at(0) == ' ') {
         unsigned it = 0;
@@ -84,18 +60,18 @@ bool Pipe::evaluate() {
     int status; // Create location in memory for which waitpid status info 
                     // is stored in
 
-    // 5) overwrite standard input -- SAVE
+    int saveOut = dup(1);
+    if (saveOut == -1) {
+        perror("dup out");
+        return false;
+    }
+
     int saveIn = dup(0);
     if (saveIn == -1) {
         perror("dup in");
         return false;
     }
-    if (dup2(pipe1[0], 0) == -1) {
-        perror("in dup2");
-        return false;
-    }
 
-    // 6) execute right
     pid_t pid = fork(); // Fork() so execvp() doesn't quit program
 
     // If error with fork
@@ -106,10 +82,38 @@ bool Pipe::evaluate() {
 
     // Child process, calls execvp()
     if (pid == 0) {
+        // 2) overwrite standard output -- SAVE OUTPUT
+        if (dup2(pipe1[1], 1) == -1) {
+            perror("out dup2");
+            return false;
+        }
+
+        // 3) run left
+        bool leftside = left->evaluate();
+
+        if (!leftside) {
+            return false;
+        }
+
+        // 4) restore standard output
+        if (dup2(saveOut, 1) == -1) {
+            perror("leftside restore");
+            return false;
+        }
+
+        // 5) overwrite standard input -- SAVE
+        if (dup2(pipe1[0], 0) == -1) {
+            perror("in dup2");
+            return false;
+        }
+
+        // Execute right side
         if (execvp(args[0], args) == -1) {
             perror("exec");
             exit(1);
         }
+        // Unconditional kill
+        exit(1);
     }
 
     // Deallocating char** b/c no longer needed
@@ -122,7 +126,6 @@ bool Pipe::evaluate() {
             perror("rightside restore");
             return false;
         }
-
         if (close(pipe1[0]) == -1) {
             perror("close 0");
             return false;
@@ -131,7 +134,9 @@ bool Pipe::evaluate() {
             perror("close 1");
             return false;
         }
-        waitpid(pid, &status, 0);
+
+        // waitpid(pid, &status, 0);
+
         if (status > 0) // If status returned, execvp failed
             return false;
         else if (WEXITSTATUS(status) == 0)  // Successful execution
